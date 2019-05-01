2de58673f28367035dee933c47ae4457b1f89304
jorgepj99 <jorge.pablo.9@gmail.com>
2019-05-01 22:36:17 +0200

    Todo el ToDo

---
+++ app/tpl/footer_common.php
@@ -0,0 +1,8 @@
+            <p>
+                <a href="<?= $this->pro.'index.php/task/cerrarSesion'?>" class="btn btn-danger btn-md">Cerrar sesión</a>
+            </p>
+        </div>
+        <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script src="public/js/app.js"></script>
+    </body>
+</html>
